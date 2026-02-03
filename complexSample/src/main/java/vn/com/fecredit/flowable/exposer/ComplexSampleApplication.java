e9aa854973d8603065fa760acc46df5d61e68f4d
dnguyenminh@gmail.com <dnguyenminh@gmail.com>
2026-02-04 00:47:54 +0700

    feat: add complexSample web module

---
+++ complexSample/src/main/java/vn/com/fecredit/flowable/exposer/ComplexSampleApplication.java
@@ -0,0 +1,11 @@
+package vn.com.fecredit.flowable.exposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexSampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComplexSampleApplication.class, args);
+    }
+}
